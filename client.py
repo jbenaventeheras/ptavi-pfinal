b3d0d099a4b08f46c80dc21f16b35e51abb2d25a
jbenaventeheras <javierbenavente@MacBook.local>
2019-06-04 02:43:28 +0200

    añado begin al log.txt

--- client.py
+++ client.py
@@ -13,13 +13,7 @@ from xml.sax.handler import ContentHandler
 if len(sys.argv) != 4:
         sys.exit('Usage :python3 uaclient.py config method option')
 
-xml_config = sys.argv[1]
 
-if os.path.exists(xml_config):
-    method = str.upper(sys.argv[2])
-    option = str.upper(sys.argv[3])
-else:
-    sys.exit('fichero XML no encontrado')
 
 class XMLHandlerClient(ContentHandler):
 
@@ -45,9 +39,8 @@ class XMLHandlerClient(ContentHandler):
 
 def ReadXmlClient(xml_config):
 
-    xml_config = sys.argv[1]
-    try:
 
+    try:
         parser = make_parser()
         Handler = XMLHandlerClient()
         parser.setContentHandler(Handler)
@@ -55,27 +48,62 @@ def ReadXmlClient(xml_config):
         configtags = Handler.get_att()
 
     except FileNotFoundError:
-        sys.exit('File not found')
+        sys.exit('fichero XML no encontrado')
 
     return configtags
 
+class Client_Log:
+
+    def __init__(self, file_log):
+
+        if not os.path.exists(file_log):
+            os.system('touch ' + file_log)
+        self.file = file_log
+
+
+    def Begin_client(self):
+        Hora_inicio = time.strftime("%Y%m%d%H%M%S ", time.gmtime(time.time()))
+        mensaje_inicio = Hora_inicio + ' Starting...\n'
+        log_write = open(self.file, 'a')
+        log_write.write(mensaje_inicio)
+        log_write.close()
+
+
+
 if __name__ == "__main__":
 
-    client_tags = ReadXmlClient(xml_config)
-    username = client_tags[0][1]
+    try:
+            xml_config = sys.argv[1]
+            client_tags = ReadXmlClient(xml_config)
+            username = client_tags[0][1]
 
-    passwd = client_tags[0][1]['passwd']
-    uaserv_ip = client_tags[1][1]['ip']
-    uaserv_port = str(client_tags[1][1]['puerto'])
-    audio_port = (client_tags[2][1]['puerto'])
-    SERVER_Proxy = client_tags[3][1]['ip']
-    PORT_Proxy = int(client_tags[3][1]['puerto'])
-    file_log = client_tags[4][1]['path']
-    audio = client_tags[5][1]['path']
+            passwd = client_tags[0][1]['passwd']
+            uaserv_ip = client_tags[1][1]['ip']
+            uaserv_port = str(client_tags[1][1]['puerto'])
+            audio_port = (client_tags[2][1]['puerto'])
+            SERVER_Proxy = client_tags[3][1]['ip']
+            PORT_Proxy = int(client_tags[3][1]['puerto'])
+            file_log = client_tags[4][1]['path']
+            audio = client_tags[5][1]['path']
+            Loggin = Client_Log(file_log)
+            Loggin.Begin_client()
 
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
-        my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        my_socket.connect((SERVER_Proxy, PORT_Proxy))
-        data = my_socket.recv(1024)
-        server_OK = data.decode('utf-8').split(' ')[-1]
-        print('RECIBIDO EN SOCKET MENSAJE: ' + data.decode('utf-8')
+
+            with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+                my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+                my_socket.connect((SERVER_Proxy, PORT_Proxy))
+
+                print('RECIBIDO EN SOCKET MENSAJE:')
+
+    except IndexError:
+        print('SIP/2.0 404 User Not Found\r\n\r\n')
+        sys.exit()
+
+
+    except ConnectionRefusedError:
+        print("No server listening at " + SERVER_Proxy + ' port ' + str(PORT_proxy))
+        sys.exit()
+
+    except KeyboardInterrupt:
+        print("client finsh")
+        sys.exit()
